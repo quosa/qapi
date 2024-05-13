3ad1c21ff0581732fffee5121b1f7c7e4869c0c2
Jussi Kuosa <jussi.kuosa@iki.fi>
2024-05-13 21:03:01 +0300

    v1 API handler to clean up route definitions

--- go_net_http/main.go
+++ go_net_http/main.go
@@ -5,9 +5,20 @@ import (
 	"net/http"
 )
 
+type v1APIHandler struct{}
+
+func (h *v1APIHandler) getAllTasks(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "[]")
+}
+
 func main() {
 	var mux = http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	var v1 = &v1APIHandler{}
+
+	mux.HandleFunc("GET /v1/tasks/", v1.getAllTasks)
+	// NOTE: {$} to match only root (others get 404)
+	mux.HandleFunc("/{$}", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprintf(w, "Welcome to QAPI!")
 	})
@@ -18,11 +29,42 @@ func main() {
 }
 
 /*
+
+// Welcome message only in root / path
 curl -i http://localhost:8080/
 HTTP/1.1 200 OK
-Date: Mon, 13 May 2024 17:25:21 GMT
+Date: Mon, 13 May 2024 17:58:23 GMT
 Content-Length: 16
 Content-Type: text/plain; charset=utf-8
 
 Welcome to QAPI!
+
+// Empty array in /v1/tasks/ path (wrong content type still)
+curl -i http://localhost:8080/v1/tasks/
+HTTP/1.1 200 OK
+Date: Mon, 13 May 2024 17:58:37 GMT
+Content-Length: 2
+Content-Type: text/plain; charset=utf-8
+
+[]
+
+// 404 for non-existing v2 path
+curl -i http://localhost:8080/v2/tasks
+HTTP/1.1 404 Not Found
+Content-Type: text/plain; charset=utf-8
+X-Content-Type-Options: nosniff
+Date: Mon, 13 May 2024 17:58:53 GMT
+Content-Length: 19
+
+404 page not found
+
+// 404 for nonsensical paths
+curl -i http://localhost:8080/foo
+HTTP/1.1 404 Not Found
+Content-Type: text/plain; charset=utf-8
+X-Content-Type-Options: nosniff
+Date: Mon, 13 May 2024 17:59:00 GMT
+Content-Length: 19
+
+404 page not found
 */
